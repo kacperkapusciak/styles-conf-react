deb0c1c55939f8d0a25f48d13506e2000d294658
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-12 12:54:29 +0100

    Added Teasers section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Container from "components/UI/Container/Container";
 import Header from "components/UI/Header/Header";
 import Hero from "components/Home/Hero/Hero";
+import Teasers from "components/Home/Teasers/Teasers";
 
 const Home = props => (
-  <Container>
-    <Header />
-    <Hero />
-  </Container>
+  <>
+    <Container>
+      <Header />
+      <Hero />
+    </Container>
+    <Teasers />
+  </>
 );
 
 export default Home;
