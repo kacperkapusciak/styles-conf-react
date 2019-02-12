ce02c8fd0ae76c7e5af17cad425ccadd2bc72590
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-12 13:18:13 +0100

    Bugfix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const app = (
-  <BrowserRouter basename="styles-conf-react">
+  <BrowserRouter basename="/styles-conf-react">
     <App />
   </BrowserRouter>
 );
