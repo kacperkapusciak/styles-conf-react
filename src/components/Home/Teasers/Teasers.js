658eab56cc41705c8c9e9db991920239afdb350d
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-13 13:28:51 +0100

    Improvement

--- src/components/Home/Teasers/Teasers.js
+++ src/components/Home/Teasers/Teasers.js
@@ -7,10 +7,7 @@ export default () => (
   <section className={styles.teasers}>
     <div className={styles.grid}>
       {teasers.map((teaser, i) => (
-        <Teaser
-          key={`teaser-${i}`}
-          {...teaser}
-        />
+        <Teaser key={`teaser-${i}`} {...teaser} />
       ))}
     </div>
   </section>
