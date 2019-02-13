ccbadcdf394086f0c76c6a4fd54d319451218838
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-02-13 11:34:18 +0100

    Implementing schedule page (#3)
    * Added Lead component
    
    * Added Timetable component
    
    * Implemented Schedule page

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Container from "components/UI/Container/Container";
+import Header from "components/UI/Header/Header";
+import Footer from "components/UI/Footer/Footer";
+import Lead from "components/UI/Lead/Lead";
+import Timetable from "components/Schedule/Timetable/Timetable";
 
 const Schedule = props => (
-  <div>
-    <p>Schedule</p>
-  </div>
+  <>
+    <Container>
+      <Header />
+    </Container>
+
+    <Lead
+      title="Schedule"
+      text="The conference opens with amazing workshops and continues with two days of incredible talks and keynotes, all of which are facilitated by industry-leading experts."
+    />
+
+    <Timetable />
+
+    <Container>
+      <Footer />
+    </Container>
+  </>
 );
 
 export default Schedule;
