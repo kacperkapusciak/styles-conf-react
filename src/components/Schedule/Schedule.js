80a2c28d8f868fc2ffd1a29b86318be7d70652e8
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-13 11:26:13 +0100

    Implemented Schedule page

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -3,6 +3,7 @@ import Container from "components/UI/Container/Container";
 import Header from "components/UI/Header/Header";
 import Footer from "components/UI/Footer/Footer";
 import Lead from "components/UI/Lead/Lead";
+import Timetable from "components/Schedule/Timetable/Timetable";
 
 const Schedule = props => (
   <>
@@ -14,6 +15,8 @@ const Schedule = props => (
       title="Schedule"
       text="The conference opens with amazing workshops and continues with two days of incredible talks and keynotes, all of which are facilitated by industry-leading experts."
     />
+
+    <Timetable />
 
     <Container>
       <Footer />
