9382360e9c302553061947c157719ff1ef9de44f
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-14 12:11:22 +0100

    Prevent website from 'jumping' while loading images

--- src/components/Home/Teasers/Teaser/Teaser.js
+++ src/components/Home/Teasers/Teaser/Teaser.js
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 import styles from "components/Home/Teasers/Teaser/Teaser.module.css";
 
-const uri = "https://firebasestorage.googleapis.com/v0/b/styles-conf-react.appspot.com/o/";
+const uri =
+  "https://firebasestorage.googleapis.com/v0/b/styles-conf-react.appspot.com/o/";
 
- const Teaser = ({ title, image, subtitle, text }) => (
+const Teaser = ({ title, image, subtitle, text }) => (
   <section className={styles.teaser}>
     <h5>{title}</h5>
     <NavLink to={`/${title}`}>
-      <img src={uri + image} alt="" />
+      <div className={styles.img_container}>
+        <img src={uri + image} alt="" />
+      </div>
       <h3>{subtitle}</h3>
     </NavLink>
     <p>{text}</p>
@@ -21,6 +24,6 @@ Teaser.propTypes = {
   image: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired
-}
+};
 
 export default Teaser;
