0206502e00081dd115fe13bd9d1f46d8859ddda9
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-14 18:24:37 +0100

    Implemented Speakers page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Container from "components/UI/Container/Container";
 import Header from "components/UI/Header/Header";
 import Hero from "components/Home/Hero/Hero";
 import Teasers from "components/Home/Teasers/Teasers";
