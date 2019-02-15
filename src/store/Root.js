8f783fc124b60deb7632b6de015531d76a988f8d
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-15 21:37:40 +0100

    Switched redux-promise for redux-thunk and added redux devtools

--- src/store/Root.js
+++ src/store/Root.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import reducers from "store/reducers";
 
 export default ({ children, initialState = {} }) => {
+  
+  const composeEnhancers =
+    process.env.NODE_ENV === "development"
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      : null || compose;
+
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    composeEnhancers(applyMiddleware(thunk))
   );
 
-  return <Provider store={store}>{children}</Provider>
+  return <Provider store={store}>{children}</Provider>;
 };
