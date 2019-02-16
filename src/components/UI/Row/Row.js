8f29f6327a2dc4de8990e4a16d268fe8ed77733e
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-16 18:36:32 +0100

    Little semantics improvement

--- src/components/UI/Row/Row.js
+++ src/components/UI/Row/Row.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "components/UI/Row/Row.module.css";
 
-const Row = ({ children }) => <div className={styles.row}>{children}</div>;
+const Row = ({ children }) => <section className={styles.row}>{children}</section>;
 
 export default Row;
