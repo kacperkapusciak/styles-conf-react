95bf532c78506a822a11a420ffbed075c1d55501
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-18 22:48:34 +0100

    Updates

---
+++ precache-manifest.79c1cf2ad94466400b25ca9fb131a7ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d01cf968eac73cd6e942",
+    "url": "/styles-conf-react/static/js/runtime~main.d01cf968.js"
+  },
+  {
+    "revision": "e8f78249bbeac8fb9dd1",
+    "url": "/styles-conf-react/static/js/main.e8f78249.chunk.js"
+  },
+  {
+    "revision": "509fd883ce2608cf675a",
+    "url": "/styles-conf-react/static/js/1.509fd883.chunk.js"
+  },
+  {
+    "revision": "e8f78249bbeac8fb9dd1",
+    "url": "/styles-conf-react/static/css/main.9f15fa72.chunk.css"
+  },
+  {
+    "revision": "c8694208474e9552ec7ed1fe49d8ed0a",
+    "url": "/styles-conf-react/index.html"
+  }
+];
